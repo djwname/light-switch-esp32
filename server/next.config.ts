d2b68aabefc8264ac379afda1cf0ddbb71a5c0f3
lyle <your.email@example.com>
2025-10-26 00:28:44 +0800

    add domain

--- server/next.config.ts
+++ server/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["pi.dj-w.com", "pi"],
 };
 
 export default nextConfig;
